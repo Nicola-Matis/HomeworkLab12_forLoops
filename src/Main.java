adf4ca5313035434b61aef3f77a929765a57d873
Nicola-Matis <114828368+Nicola-Matis@users.noreply.github.com>
2022-11-06 17:04:49 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LogicalOp logicalOp = new LogicalOp();
+
+        //1
+
+        logicalOp.FromNumberTo100(95);
+
+        //2
+
+        logicalOp.FromNumberToMinus100(-95);
+
+        //3
+
+        logicalOp.FromNumberToAnotherNumber(6,15);
+
+        //4
+
+        logicalOp.ez(10,5);
+
+
+        //5
+
+        logicalOp.EvenTo100(90);
+
+        //6
+
+        logicalOp.OddTo100(92);
+
+        //7
+
+        logicalOp.SumTo100(90);
+
+        //8
+
+        logicalOp.AverageTo100(5);
+
+        //9
+        
+        System.out.println("-");
     }
 }
